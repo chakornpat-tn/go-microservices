6cc5f17278d3de7f4833241a9e0dfcfa77422995
chakornpat <chakornpat0206@outlook.com>
2025-04-24 20:11:03 +0700

    Add configuration structure and load function for environment variables

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"os"
 
+	"github.com/chakornpat-tn/go-microservices/config"
+)
+
+func main() {
+	ctx := context.Background()
+	_ = ctx
+
+	//Init Env Config
+	cfg := config.LoadConfig(
+		func() string {
+			if len(os.Args) < 2 {
+				log.Fatal("Please provide the config file path")
+			}
+			return os.Args[1]
+		}(),
+	)
+
+	log.Println(cfg)
 }
