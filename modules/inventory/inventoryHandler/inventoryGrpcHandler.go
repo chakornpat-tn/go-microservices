7e33d117a7e686eeb8cd44377ae00217f87cf7b6
chakornpat <chakornpat0206@outlook.com>
2025-04-25 21:18:10 +0700

    feat: Implement inventory module structure with gRPC, HTTP handlers, and repository interfaces

--- modules/inventory/inventoryHandler/inventoryGrpcHandler.go
+++ modules/inventory/inventoryHandler/inventoryGrpcHandler.go
@@ -1,1 +1,15 @@
 package inventoryHandler
+
+import "github.com/chakornpat-tn/go-microservices/modules/inventory/inventoryUsecase"
+
+type (
+	inventoryGrpcHandler struct {
+		inventoryUsecase inventoryUsecase.InventoryUsecaseService
+	}
+)
+
+func NewInventoryGrpcHandler(inventoryUsecase inventoryUsecase.InventoryUsecaseService) *inventoryGrpcHandler {
+	return &inventoryGrpcHandler{
+		inventoryUsecase: inventoryUsecase,
+	}
+}
--- modules/inventory/inventoryUsecase/inventoryUsecase.go
+++ modules/inventory/inventoryUsecase/inventoryUsecase.go
@@ -1,1 +1,17 @@
 package inventoryUsecase
+
+import "github.com/chakornpat-tn/go-microservices/modules/inventory/inventoryRepository"
+
+type (
+	InventoryUsecaseService interface{}
+
+	inventoryUsecase struct {
+		inventoryRepo inventoryRepository.InventoryRepositoryService
+	}
+)
+
+func NewInventoryUsecase(inventoryRepo inventoryRepository.InventoryRepositoryService) InventoryUsecaseService {
+	return &inventoryUsecase{
+		inventoryRepo: inventoryRepo,
+	}
+}
