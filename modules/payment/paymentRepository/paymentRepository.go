76a3fd96562eb9823605d6858f6404bb16aa3377
chakornpat <chakornpat0206@outlook.com>
2025-04-25 21:30:22 +0700

    feat: Implement payment module structure with HTTP handlers and repository interfaces

--- modules/payment/paymentRepository/paymentRepository.go
+++ modules/payment/paymentRepository/paymentRepository.go
@@ -1,1 +1,26 @@
 package paymentRepository
+
+import (
+	"context"
+
+	"github.com/chakornpat-tn/go-microservices/config"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type (
+	PaymentRepositoryService interface{}
+
+	paymentRepository struct {
+		db *mongo.Client
+	}
+)
+
+func NewPaymentRepository(db *mongo.Client) PaymentRepositoryService {
+	return &paymentRepository{
+		db: db,
+	}
+}
+
+func (r *paymentRepository) paymentDbConn(pctx context.Context, cfg *config.Config) *mongo.Database {
+	return r.db.Database("payment_db")
+}
--- modules/payment/paymentUsecase/paymentUsecase.go
+++ modules/payment/paymentUsecase/paymentUsecase.go
@@ -1,1 +1,17 @@
 package paymentUsecase
+
+import "github.com/chakornpat-tn/go-microservices/modules/payment/paymentRepository"
+
+type (
+	PaymentUsecaseService interface{}
+
+	paymentUsecase struct {
+		paymentRepo paymentRepository.PaymentRepositoryService
+	}
+)
+
+func NewPaymentUsecase(paymentRepo paymentRepository.PaymentRepositoryService) PaymentUsecaseService {
+	return &paymentUsecase{
+		paymentRepo: paymentRepo,
+	}
+}
