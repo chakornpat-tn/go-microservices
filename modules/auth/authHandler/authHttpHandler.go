cd5d91ebaac00465b42e6cb4c1d9511a7b120f72
chakornpat <chakornpat0206@outlook.com>
2025-04-25 20:12:25 +0700

    feat: Implement auth module structure with repository, usecase, and handler interfaces

--- modules/auth/authHandler/authHttpHandler.go
+++ modules/auth/authHandler/authHttpHandler.go
@@ -1,1 +1,15 @@
 package authHandler
+
+import "github.com/chakornpat-tn/go-microservices/modules/auth/authUsecase"
+
+type (
+	authGrpcHandler struct {
+		authUsecase authUsecase.AuthUsecaseService
+	}
+)
+
+func NewAuthGrpcHandler(authUsecase authUsecase.AuthUsecaseService) authUsecase.AuthUsecaseService {
+	return &authGrpcHandler{
+		authUsecase: authUsecase,
+	}
+}
--- modules/auth/authUsecase/authUsecase.go
+++ modules/auth/authUsecase/authUsecase.go
@@ -1,1 +1,18 @@
 package authUsecase
+
+import "github.com/chakornpat-tn/go-microservices/modules/auth/authRepository"
+
+type (
+	AuthUsecaseService interface {
+	}
+
+	authUsecase struct {
+		authRepo authRepository.AuthRepositoryService
+	}
+)
+
+func NewAuthUsecase(authRepo authRepository.AuthRepositoryService) AuthUsecaseService {
+	return &authUsecase{
+		authRepo: authRepo,
+	}
+}
