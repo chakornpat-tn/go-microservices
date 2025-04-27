e6249664c71aa3754165a02365cbae4e31f3b644
chakornpat <chakornpat0206@outlook.com>
2025-04-27 18:15:17 +0700

    feat: define Inventory struct and related request types for inventory management

--- modules/inventory/inventoryModel.go
+++ modules/inventory/inventoryModel.go
@@ -1,1 +1,23 @@
 package inventory
+
+import (
+	"github.com/chakornpat-tn/go-microservices/modules/item"
+	"github.com/chakornpat-tn/go-microservices/modules/models"
+)
+
+type (
+	UpdateInventoryReq struct {
+		PlayerID string `json:"player_id" validate:"required, max=64"`
+		ItemId   string `json:"item_id" validate:"required, max=64"`
+	}
+
+	ItemInInventory struct {
+		InventoryID string `json:"inventory_id"`
+		*item.ItemShowCase
+	}
+
+	PlayerInventory struct {
+		PlayerID string `json:"player_id"`
+		*models.PaginateRes
+	}
+)
