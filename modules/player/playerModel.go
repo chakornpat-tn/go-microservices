6e29afea832f1f92df9fddd469d6b5c5b92e8cac
chakornpat <chakornpat0206@outlook.com>
2025-04-27 16:40:31 +0700

    feat: define Credential and Role types in authEntity, add request structs in authModel, and update playerModel package declaration

--- modules/player/playerModel.go
+++ modules/player/playerModel.go
@@ -1,1 +1,13 @@
-package auth
+package player
+
+import "time"
+
+type (
+	PlayerProfile struct {
+		ID        string    `json:"_id"`
+		Email     string    `json:"email"`
+		Username  string    `json:"username"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+	}
+)
