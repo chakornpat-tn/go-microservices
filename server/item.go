386d260c7b84f915f7550caad984b5a01d69c3a4
chakornpat <chakornpat0206@outlook.com>
2025-04-29 19:45:19 +0700

    feat: add gRPC server initialization for auth, inventory, item, and player services

--- server/item.go
+++ server/item.go
@@ -1,9 +1,13 @@
 package server
 
 import (
+	"log"
+
 	"github.com/chakornpat-tn/go-microservices/modules/item/itemHandler"
+	itemPb "github.com/chakornpat-tn/go-microservices/modules/item/itemPb"
 	"github.com/chakornpat-tn/go-microservices/modules/item/itemRepository"
 	"github.com/chakornpat-tn/go-microservices/modules/item/itemUsecase"
+	"github.com/chakornpat-tn/go-microservices/pkg/grpccon"
 )
 
 func (s *server) itemService() {
@@ -13,7 +17,13 @@ func (s *server) itemService() {
 	grpcHandler := itemHandler.NewItemGrpcHandler(usecase)
 
 	_ = httpHandler
-	_ = grpcHandler
+
+	go func() {
+		grpcServer, lis := grpccon.NewGrpcServer(&s.cfg.Jwt, s.cfg.Grpc.ItemUrl)
+		itemPb.RegisterItemGrpcServiceServer(grpcServer, grpcHandler)
+		log.Printf("Item gRPC server listening on %s", s.cfg.Grpc.ItemUrl)
+		grpcServer.Serve(lis)
+	}()
 
 	item := s.app.Group("/item_v1")
 
