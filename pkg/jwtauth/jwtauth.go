d5721262e19ef3e7ee606757806c2eb6f1fcde13
chakornpat <chakornpat0206@outlook.com>
2025-05-07 18:17:35 +0700

    feat: enhance JWT token handling with time management functions

--- pkg/jwtauth/jwtauth.go
+++ pkg/jwtauth/jwtauth.go
@@ -1,6 +1,11 @@
 package jwtauth
 
-import "github.com/golang-jwt/jwt/v5"
+import (
+	"math"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type (
 	AuthFactory interface {
@@ -31,8 +36,41 @@ type (
 	apiKey struct{ *authConcrete }
 )
 
+func now() time.Time {
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+	return time.Now().In(loc)
+}
+
 func (a *authConcrete) SignToken() string {
 	token := jwt.NewWithClaims(jwt.SigningMethodES256, a.Claims)
 	ss, _ := token.SignedString(a.Secret)
 	return ss
+}
+
+// Note: t is second unit
+func JwtTimeDurationCal(t int64) *jwt.NumericDate {
+	return jwt.NewNumericDate(now().Add(time.Duration(t * int64(math.Pow10(9)))))
+}
+
+func jwtTimeRepeatAdapter(t int64) *jwt.NumericDate {
+	return jwt.NewNumericDate(time.Unix(t, 0))
+}
+
+func NewAccessToken(secret string, expiredAt int64, claims *Claims) AuthFactory {
+	return &access_token{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claims: claims,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "go-microservices",
+					Subject:   "access-token",
+					Audience:  []string{"go-microservices"},
+					ExpiresAt: JwtTimeDurationCal(expiredAt),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
 }
