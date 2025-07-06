89a69ff1de8ea554b4248e278ae18575b3b59bc0
ChildNam <chakornpat0206@outlook.com>
2025-07-06 12:37:51 +0700

    feat: implement FindItemsInIDs method in payment usecase and repository

--- modules/payment/paymentRepository/paymentRepository.go
+++ modules/payment/paymentRepository/paymentRepository.go
@@ -2,13 +2,21 @@ package paymentRepository
 
 import (
 	"context"
+	"errors"
+	"log"
+	"time"
 
 	"github.com/chakornpat-tn/go-microservices/config"
+	itemPb "github.com/chakornpat-tn/go-microservices/modules/item/itemPb"
+	"github.com/chakornpat-tn/go-microservices/pkg/grpccon"
+	"github.com/chakornpat-tn/go-microservices/pkg/jwtauth"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type (
-	PaymentRepositoryService interface{}
+	PaymentRepositoryService interface {
+		FindItemsInIDs(pctx context.Context, grpcUrl string, req *itemPb.FindItemsInIdsReq) (*itemPb.FindItemsInIdsRes, error)
+	}
 
 	paymentRepository struct {
 		db *mongo.Client
@@ -23,4 +31,30 @@ func NewPaymentRepository(db *mongo.Client) PaymentRepositoryService {
 
 func (r *paymentRepository) paymentDbConn(pctx context.Context, cfg *config.Config) *mongo.Database {
 	return r.db.Database("payment_db")
+}
+
+func (r *paymentRepository) FindItemsInIDs(pctx context.Context, grpcUrl string, req *itemPb.FindItemsInIdsReq) (*itemPb.FindItemsInIdsRes, error) {
+	ctx, cancel := context.WithTimeout(pctx, 30*time.Second)
+	defer cancel()
+
+	conn, err := grpccon.NewGrpccClient(grpcUrl)
+	if err != nil {
+		log.Printf("Error: grpc client connection failed: %s", err.Error())
+		return nil, errors.New("error:grpc connection failed")
+	}
+
+	jwtauth.SetApiKeyInContext(&ctx)
+
+	result, err := conn.Item().FindItemsInIds(ctx, req)
+	if err != nil {
+		log.Printf("Error: gRPC FindItemsInIds failed: %s", err.Error())
+		return nil, errors.New("error:gRPC find items in ids failed")
+	}
+
+	if len(result.Items) == 0 {
+		log.Printf("Error: gRPC FindItemsInIds failed")
+		return nil, errors.New("error:gRPC find items in ids failed")
+	}
+
+	return result, nil
 }
--- modules/payment/paymentUsecase/paymentUsecase.go
+++ modules/payment/paymentUsecase/paymentUsecase.go
@@ -1,9 +1,20 @@
 package paymentUsecase
 
-import "github.com/chakornpat-tn/go-microservices/modules/payment/paymentRepository"
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/chakornpat-tn/go-microservices/modules/item"
+	itemPb "github.com/chakornpat-tn/go-microservices/modules/item/itemPb"
+	"github.com/chakornpat-tn/go-microservices/modules/payment"
+	"github.com/chakornpat-tn/go-microservices/modules/payment/paymentRepository"
+)
 
 type (
-	PaymentUsecaseService interface{}
+	PaymentUsecaseService interface {
+		FindItemsInIDs(pctx context.Context, grpcUrl string, req []*payment.ItemServiceReqDatum) error
+	}
 
 	paymentUsecase struct {
 		paymentRepo paymentRepository.PaymentRepositoryService
@@ -14,4 +25,48 @@ func NewPaymentUsecase(paymentRepo paymentRepository.PaymentRepositoryService) P
 	return &paymentUsecase{
 		paymentRepo: paymentRepo,
 	}
+}
+
+func (u *paymentUsecase) FindItemsInIDs(pctx context.Context, grpcUrl string, req []*payment.ItemServiceReqDatum) error {
+	setIDs := make(map[string]bool)
+	for _, v := range req {
+		if !setIDs[v.ItemID] {
+			setIDs[v.ItemID] = true
+		}
+	}
+
+	itemData, err := u.paymentRepo.FindItemsInIDs(pctx, grpcUrl, &itemPb.FindItemsInIdsReq{
+		Ids: func() []string {
+			itemIds := make([]string, 0)
+			for k := range setIDs {
+				itemIds = append(itemIds, k)
+			}
+			return itemIds
+		}(),
+	})
+	if err != nil {
+		log.Printf("Error: FindItemsInIDs failed: %s", err.Error())
+		return errors.New("error: item not found")
+	}
+
+	itemMaps := make(map[string]*item.ItemShowCase)
+	for _, v := range itemData.Items {
+		itemMaps[v.Id] = &item.ItemShowCase{
+			ItemID:   v.Id,
+			Title:    v.Title,
+			Price:    v.Price,
+			ImageUrl: v.ImgUrl,
+			Damage:   int(v.Damage),
+		}
+	}
+
+	for i := range req {
+		if _, ok := itemMaps[req[i].ItemID]; !ok {
+			log.Printf("Error: item not found: %s", req[i].ItemID)
+			return errors.New("error: item not found")
+		}
+		req[i].Price = itemMaps[req[i].ItemID].Price
+	}
+
+	return nil
 }
